454f966bc3f4367c465326a038fb67707a7602ba
Jan Vleeshouwers <jan@AgendaJan.(none)>
2012-11-19 22:54:15 +0100

    Update header

--- backend/pieusb.c
+++ backend/pieusb.c
@@ -1,12 +1,52 @@
+/* sane - Scanner Access Now Easy.
+
+   pieusb.c
+
+   Copyright (C) 2012 Jan Vleeshouwers, Michael Rickmann, Klaus Kaempf
+
+   This file is part of the SANE package.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+   MA 02111-1307, USA.
+
+   As a special exception, the authors of SANE give permission for
+   additional uses of the libraries contained in this release of SANE.
+
+   The exception is that, if you link a SANE library with other files
+   to produce an executable, this does not by itself cause the
+   resulting executable to be covered by the GNU General Public
+   License.  Your use of that executable is in no way restricted on
+   account of linking the SANE library code into it.
+
+   This exception does not, however, invalidate any other reasons why
+   the executable file might be covered by the GNU General Public
+   License.
+
+   If you submit changes to SANE to the maintainers to be included in
+   a subsequent release, you agree by submitting the changes that
+   those changes may be distributed with this exception intact.
+
+   If you write modifications of your own for SANE, it is your choice
+   whether to permit this exception to apply to your modifications.
+   If you do not wish that, delete this exception notice.  */
+
 /* 
- * File:   pieusb.c
- * Author: jan
- *
- * Created on July 22, 2012, 2:22 PM
- * 
- * SANE interface to two Reflecta USB scanners:
- * - CrystalScan 7200 (model id 0x30)
- * - ProScan 7200 (model id 0x36)
+ * SANE interface to three Reflecta USB scanners with USB-id 0x05e3/0x0145:
+ * - Reflecta CrystalScan 7200 (model id 0x30)
+ * - Reflecta ProScan 7200 (model id 0x36)
+ * - Reflecta 6000 Multiple Slide Scanner (model id 0x3A)
  */
 
 /* --------------------------------------------------------------------------
@@ -179,7 +219,7 @@ sane_init (SANE_Int * version_code, SANE_Auth_Callback __sane_unused__ authorize
     /* Reflecta 6000 Multiple Slide Scanner */
     pieusb_supported_usb_device_list[1].vendor = 0x05e3;
     pieusb_supported_usb_device_list[1].product = 0x0142;
-    pieusb_supported_usb_device_list[1].model = 0x00; /* to be determined */
+    pieusb_supported_usb_device_list[1].model = 0x3A; /* to be determined */
     /* end of list */
     pieusb_supported_usb_device_list[2].vendor = 0;
     pieusb_supported_usb_device_list[2].product = 0;
